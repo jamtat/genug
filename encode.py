e7c4e3c10ebb2238fe9a13bcb6d09f6cde107fbd
Ben Tattersley <ubershinysheep@gmail.com>
2016-01-08 18:13:17 +0000

    Formatting and hopefully faster implementation

--- encode.py
+++ encode.py
@@ -1,48 +1,57 @@
-import os, sys, multiprocessing
+import os
+import sys
+import multiprocessing
 from PIL import Image
 
 im = Image.open('olivia.jpg')
+im2 = Image.open('test.png')
 
 # Per pixel transform of an image
+
+
 def pixelTransform(image, *transforms):
 
-	img = Image.new( 'RGBA', image.size )
-	img.paste( image )
-	(w, h) = img.size
-
+	(w, h) = image.size
+	outImg = Image.new('RGBA', image.size)
+	outPixels = list(image.getdata())
+	# Transforms
 	for transform in transforms:
 		for x in range(w):
 			for y in range(h):
-				coord = (x, y)
-				img.putpixel( coord, tuple( transform( img.getpixel( coord ) ) ) )
+				outPixels[y * w + x] = tuple(transform(outPixels[y * w + x]))
 
-	return img
+	outImg.putdata( outPixels )
+
+	return outImg
+
 
 # Invert an image but preserve alpha
 def invert(pixel):
-	return [255 - i for i in pixel[:3]]
+	return [255 - i for i in pixel[:3]] + [(pixel[3] if len(pixel) > 3 else 255)]
+
 
 # Invert the alpha channel
 def invert_alpha(pixel):
 	return list(pixel[:3]) + [255 - pixel[3]]
 
+
 # Create a luminanace map of an image
 def greyscale(pixel):
-	intensity = int(( pixel[0] + pixel[1] + pixel[2] ) / 3)
-	return [intensity]*3
+	intensity = int((pixel[0] + pixel[1] + pixel[2]) / 3)
+	return [intensity] * 3
 
 
 # Apply the luminance of a pixel as its alpha
 def luma_map(pixel):
-	intensity = int(( pixel[0] + pixel[1] + pixel[2] ) / 3)
-	return ([0]*3) + [intensity]
+	intensity = int((pixel[0] + pixel[1] + pixel[2]) / 3)
+	return ([0] * 3) + [intensity]
 
 
 # Return a function that fills all pixels in an image with a colour
 def fill(r=255, g=255, b=255):
 
 	def do_fill(pixel):
-		return [r, g, b] + [(pixel[3] if len(pixel)>3 else 255)]
+		return [r, g, b] + [(pixel[3] if len(pixel) > 3 else 255)]
 
 	return do_fill
 
