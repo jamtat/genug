e15280307d62db6d9d20ff1e0401b3f000244616
Ben Tattersley <ubershinysheep@gmail.com>
2016-04-19 16:42:54 +0100

    Export applicators

--- genug.js
+++ genug.js
@@ -7,11 +7,44 @@ const Worker = require( './lib/worker/worker' )
 const StreamWorker = require( './lib/worker/streamworker' )
 const WorkerServer = require( './lib/worker/workerserver' )
 
+const ApplyProcessSerial = require( './lib/applicators/serial' )
+const ApplyProcessParallel = require( './lib/applicators/parallel' )
+const ApplyProcessRemote = require( './lib/applicators/remote' )
+const ApplyProcessVolunteer = require( './lib/applicators/volunteer' )
+
+const _wrapApplicator = ( applicator ) => ( data, processFile, processArgs, applicatorArgs, callback ) => {
+
+	if ( !data ) {
+		callback( 'Error: no data supplied' )
+		return
+	}
+
+	if ( !processFile ) {
+		callback( 'Error: process required' )
+		return
+	}
+
+	processFile = path.resolve( processFile )
+
+	try {
+		fs.accessSync( processFile, fs.F_OK )
+	} catch ( e ) {
+		callback( `Error: "${processFile}" does not exist` )
+		return
+	}
+
+	applicator( data, processFile, processArgs, applicatorArgs, callback )
+}
+
+const serial = _wrapApplicator( ApplyProcessSerial )
+const parallel = _wrapApplicator( ApplyProcessParallel )
+const remote = _wrapApplicator( ApplyProcessRemote )
+const volunteer = _wrapApplicator( ApplyProcessVolunteer )
+
 module.exports = {
 	IO,
-	Worker,
-	StreamWorker,
-	WorkerServer
+	Worker, StreamWorker, WorkerServer,
+	serial, parallel, remote, volunteer
 }
 
 if ( require.main === module ) {
